816f686797d3baee8dc68ee817a0f02fa261a0c8
Lisgevan <episimo@hotmail.com>
2021-05-22 15:37:01 +0300

    toggling the model & ajaxing new posts

--- feed/views.py
+++ feed/views.py
@@ -1,7 +1,7 @@
-from django.http import request
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
 
 from .models import Post
 
@@ -33,3 +33,21 @@ class CreateNewPost(LoginRequiredMixin, CreateView):
         obj.author = self.request.user
         obj.save()
         return super().form_valid(form)
+
+    def post(self, request, *args, **kwargs):
+        
+        post = Post.objects.create(
+            text = request.POST.get("text"),
+            author = request.user,
+        )
+
+
+        return render(
+            request,
+            "includes/post.html",
+            {
+                "post": post,
+                "show_detail_link": True,
+            },
+            content_type="application/html"
+        )
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,28 +2,63 @@
 $.ajaxSetup({
 	beforeSend: function beforeSend(xhr, settings) {
 		function getCookie(name) {
-			let cookieValue = null;
+			let cookieValue = null
 
 			if (document.cookie && document.cookie !== "") {
-				const cookies = document.cookie.split(";");
+				const cookies = document.cookie.split(";")
 
 				for (let i = 0; i < cookies.length; i += 1) {
-					const cookie = jQuery.trim(cookies[i]);
+					const cookie = jQuery.trim(cookies[i])
 
 					// Does this cookie string begin with the name we want?
 					if (cookie.substring(0, name.length + 1) === `${name}=`) {
-						cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-						break;
+						cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+						break
 					}
 				}
 			}
 
-			return cookieValue;
+			return cookieValue
 		}
 
 		if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
 			// Only send the token to relative URLs i.e. locally.
-			xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"));
+			xhr.setRequestHeader("X-CSRFToken", getCookie("csrftoken"))
 		}
 	},
-});
+})
+
+$(document)
+	.on("click", ".js-toggle-modal", function (e) {
+		e.preventDefault() // to prevent default action
+		$(".js-modal").toggleClass("hidden") // toggle the class name hidden
+	})
+	.on("click", ".js-submit", function (e) {
+		e.preventDefault()
+		const text = $(".js-post-text").val().trim()
+		const $btn = $(this)
+
+		if (!text.length) {
+			return false
+		}
+
+		$btn.prop("disabled", true).text("Posting!") //select the button
+		$.ajax({
+			type: "POST",
+			url: $(".js-post-text").data("post-url"), //add data_post_url="{% url 'feed:new_post' %}" in textarea tag
+
+			data: {
+				text: text, // to make text accessible at the backend/django
+			},
+			success: dataHtml => {
+				$(".js-modal").addClass("hidden")
+				$("#posts-container").prepend(dataHtml) //add a div#posts-container in home.html
+				$btn.prop("disabled", false).text("New Post")
+				$(".js-post-text").val("")
+			},
+			error: error => {
+				console.warn(error)
+				$btn.prop("disabled", false).text("Error")
+			},
+		})
+	})
