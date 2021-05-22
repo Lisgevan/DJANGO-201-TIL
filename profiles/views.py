7660f9cdfd82f0c68b9a3be4b251bee099a64522
Lisgevan <episimo@hotmail.com>
2021-05-22 20:17:49 +0300

    profile detail view & template & posts number

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import DetailView
 
-# Create your views here.
+from feed.models import Post
+
+class ProfileDetailView(DetailView):
+    http_method_names = ['get']
+    template_name = "profiles/detail.html"
+    model = User
+    context_object_name = "user"
+    slug_field = "username"
+    slug_url_kwarg = "username"
+
+    def get_context_data(self, **kwargs):
+        user = self.get_object()
+        context = super().get_context_data(**kwargs)
+        context['total_posts'] = Post.objects.filter(author=user).count()
+        return context
+    
